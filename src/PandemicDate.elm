180219686bc336c6fd3313464fd8ccd9c0e364ca
Andrew J Lenards <andrew.lenards@gmail.com>
2022-01-13 19:58:23 -0700

    Add docstrings

--- src/PandemicDate.elm
+++ src/PandemicDate.elm
@@ -1,9 +1,29 @@
-module PandemicDate exposing (..)
+module PandemicDate exposing (toPandemicDate)
+
+{-| When you feel like time stopped and we're still in March 2020 ...
+
+
+# Function(s)
+
+@docs toPandemicDate
+
+-}
 
 import Date exposing (Date, Unit(..))
 import Time exposing (Month(..))
 
 
+{-| Returns the date in March 2020 that the
+date feels like it is.
+
+    import Time exposing (Month(..))
+    import Date exposing (Unit(..))
+    import PandemicDate
+    PandemicDate.toPandemicDate (Date.fromCalendarDate 2022 Jan 13)
+
+This will return "March 683th, 2020"
+
+-}
 toPandemicDate : Date -> String
 toPandemicDate now =
     let
